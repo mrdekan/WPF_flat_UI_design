8b26fb33aa6178dd2d8a1d1adac80e3d9252c3d0
mrdekan <yeros05040504@gmail.com>
2023-05-03 11:44:43 +0300

    Some changes

---
+++ WPF_flat_UI_design/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_flat_UI_design.Core;
+
+namespace WPF_flat_UI_design.MVVM.ViewModel
+{
+	internal class MainViewModel : ObservableObject
+	{
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
